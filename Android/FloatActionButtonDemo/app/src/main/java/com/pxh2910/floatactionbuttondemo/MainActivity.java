847f61aa2d7be5b3f00d39fc4ef6b6110d602aae
kitajima2910 <kitajima2910@gmail.com>
2024-11-18 22:34:04 +0700

    - Float Action Button Demo

---
+++ Android/FloatActionButtonDemo/app/src/main/java/com/pxh2910/floatactionbuttondemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.pxh2910.floatactionbuttondemo;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        fab = findViewById(R.id.fab);
+        fab.setOnClickListener(v -> {
+            Toast.makeText(this, "You have clicked on floating button", Toast.LENGTH_SHORT).show();
+        });
+    }
+}
